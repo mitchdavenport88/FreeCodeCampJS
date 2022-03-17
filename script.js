ef56c894307bbf8a5779870912a390fc38a1ad35
mitchdavenport88 <mitchdavenport88@hotmail.com>
2022-03-17 11:39:09 +0000

    Complete section 2 of video

--- script.js
+++ script.js
@@ -1,25 +1,63 @@
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
-sumEl = document.getElementById("sum-el")
+let player = {
+    name: "Mitch",
+    chips: 200
+}
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
 
-function add() {
-    let result = num1 + num2
-    sumEl.textContent = "Sum: " + result
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomCard = Math.floor(Math.random() * 13) + 1
+    if (randomCard > 10) {
+        return 10
+    } else if (randomCard === 1) {
+        return 11
+    } else {
+        return randomCard
+    }
 }
 
-function subtract() {
-    let result = num1 - num2
-    sumEl.textContent = "Sum: " + result
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
 }
 
-function divide() {
-    let result = num1 / num2
-    sumEl.textContent = "Sum: " + result
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
 }
 
-function multiply() {
-    let result = num1 * num2
-    sumEl.textContent = "Sum: " + result
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
 }
